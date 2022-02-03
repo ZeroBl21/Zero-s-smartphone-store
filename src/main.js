29a9fd755b9228f9838ee77ab5403387b4599d36
Zero <ZeroBl@hotmail.com>
2022-02-03 19:16:48 -0400

    Project Finished

--- src/main.js
+++ src/main.js
@@ -15,3 +15,25 @@ btnClose.addEventListener('click', () =>{
     modal.classList.remove('visible');
 });
 
+//
+
+const hideNavbarMedia = window.matchMedia("(max-width: 600px)")
+const pageTitle = document.querySelector("header .page-title");
+const header = document.querySelector("header");
+const nav = document.querySelector("header nav");
+
+hideNavbarMedia.addEventListener("change", (ev) => {
+    header.classList.toggle("active", ev.matches);
+    
+    if (!ev.matches) {
+        nav.classList.remove("unfold");
+    }
+});
+
+pageTitle.addEventListener("click", () => {
+    if (header.classList.contains("active")) {
+        nav.classList.toggle("unfold");
+    }
+});
+
+header.classList.toggle("active", hideNavbarMedia.matches);
